da8f19d4de1c7089db3d572dc799b65bd36b2f76
stutiisinha <sinhastuti200128@gmail.com>
2023-09-05 11:59:55 +0530

    changes made for serilog logging in .NET Core

--- SerilogLogging/Program.cs
+++ SerilogLogging/Program.cs
@@ -1,23 +1,42 @@
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using Serilog.Events;
+using Serilog.Sinks.File.Header;
 using SerilogLoggingApp;
+using System.Reflection;
+using Serilog.Core;
+using Serilog.Core.Enrichers;
+using Serilog.Context;
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        Log.Logger = new LoggerConfiguration()
-            .MinimumLevel.Debug()
-            .WriteTo.Console(outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.ffff} | {Level:u3} | {Properties:j} | {SourceContext} | {Message:lj}{NewLine}{Exception}")
-            .WriteTo.File("logs/logs.txt", rollingInterval: RollingInterval.Day)
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+        //Log.Logger = new LoggerConfiguration()
+        //    .WriteTo.File("logs/logs.txt", hooks: new HeaderWriter("======================================================================================================================================================{NewLine}Logging start    = {Timestamp:yyyy-MM-dd HH:mm:ss.ffff}{NewLine}Machine name     = {MachineName}{NewLine}Version          = 0.0.0.0{NewLine}AssemblyVersion  = 1.0.0.0{NewLine}Windows Version  = Windows 10 Enterprise{NewLine}----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------{NewLine}Date and Time            | Level | Product              | Module                                                     | Message{NewLine}----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------{NewLine}"))
+        //    .CreateLogger();
+        
+        Log.Logger =new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
             .CreateLogger();
+
+        //Log.Information("======================================================================================================================================================{NewLine}Logging start    = {Timestamp:yyyy-MM-dd HH:mm:ss.ffff}{NewLine}Machine name     = {MachineName}{NewLine}Version          = 0.0.0.0{NewLine}AssemblyVersion  = 1.0.0.0{NewLine}Windows Version  = Windows 10 Enterprise{NewLine}----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------{NewLine}Date and Time            | Level | Product              | Module                                                     | Message{NewLine}----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------{NewLine}");
+
+        //LogContext.PushProperty("Header", "======================================================================================================================================================{NewLine}Logging start    = {Timestamp:yyyy-MM-dd HH:mm:ss.ffff}{NewLine}Machine name     = {MachineName}{NewLine}Version          = 0.0.0.0{NewLine}AssemblyVersion  = 1.0.0.0{NewLine}Windows Version  = Windows 10 Enterprise{NewLine}----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------{NewLine}Date and Time            | Level | Product              | Module                                                     | Message{NewLine}----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------{NewLine}");
+        //Log.Logger = new LoggerConfiguration()
+        //    .MinimumLevel.Debug()
+        //    .WriteTo.Console()
+        //    .WriteTo.File("logs/logs.txt", outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.ffff} | {Level:u5} | {Properties:j} | {SourceContext} | {Message:lj}{NewLine}{Exception}")
+        //    .CreateLogger();
 
         try
         {
             CreateHostBuilder(args).Build().Run();
         }
-        catch(Exception ex)
+        catch (Exception ex)
         {
             Log.Fatal(ex, "Application startup failed");
         }
@@ -28,10 +47,16 @@ public class Program
     }
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .UseSerilog() // Adding this line to use Serilog
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+             Host.CreateDefaultBuilder(args)
+                 .ConfigureWebHostDefaults(webBuilder =>
+                 {
+                     webBuilder.UseStartup<Startup>();
+                 })
+                 .UseSerilog((hostingContext, loggerConfiguration) =>
+                 {
+                     loggerConfiguration
+                         .ReadFrom.Configuration(hostingContext.Configuration)
+                         .Enrich.WithProperty("MachineName", Environment.MachineName)
+                         .Enrich.WithProperty("Version", Assembly.GetExecutingAssembly().GetName().Version);
+                 });
 }
---
+++ SerilogLogging/SerilogHooks.cs
@@ -0,0 +1,9 @@
+﻿using Serilog.Sinks.File.Header;
+
+namespace SerilogLogging
+{
+    public class SerilogHooks
+    {
+        public static HeaderWriter MyHeaderWriter => new HeaderWriter("======================================================================================================================================================\nLogging start    = {Timestamp:yyyy-MM-dd HH:mm:ss.ffff}\nMachine name     = {MachineName}\nVersion          = 0.0.0.0\nAssemblyVersion  = 1.0.0.0\nWindows Version  = Windows 10 Enterprise\n----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\nDate and Time            | Level | Product              | Module                                                     | Message\n----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n");
+    }
+}
